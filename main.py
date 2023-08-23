dc9c5be20d45a6f701e789c4992f6527427d0823
varenh <bvarenhorst@outlook.com>
2023-08-23 14:14:12 +0200

    main loop

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import pygame
 import math
+pygame.init()
+
+HEIGHT, WIDTH = 1000, 1000
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Planet Gravity Simulation")
+
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
